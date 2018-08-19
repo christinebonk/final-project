3a3143a56b2b09ccf2d48f7937c1400521f250f0
Christine Bonk <christinebonk@gmail.com>
2018-08-18 20:43:41 -0400

    server setup

---
+++ controllers/controllers.js
@@ -0,0 +1,18 @@
+var db = require("../models");
+var bodyparser = require("body-parser");
+
+
+function routes(app) {
+	//pages
+	app.get("/", function(req, res){
+		res.render("index");
+	});
+
+	app.get("/welcome", function(req,res) {
+		res.render('home', {layout: 'onboarding.handlebars'});
+	})
+
+	//apis
+};
+
+module.exports = routes;
