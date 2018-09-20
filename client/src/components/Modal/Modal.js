d6e8aad91e17b0bb4e6d68f320242421a00a48bb
Christine Bonk <christinebonk@gmail.com>
2018-09-20 19:47:35 -0400

    dedicated to ryan for helping me find the missing select thing

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
+import API from "../../utils/App.js";
+
 
 class Modal extends Component {
 
+	state = {
+		categories: [],
+		test: 0
+	};
+
+	componentDidMount() {
+		this.retrieveBudget();
+		console.log(this.state.categories);
+	  };
+
 	retrieveBudget = () => {
-		console.log("hello");
-  }
+		API.searchBudget()
+    	.then(res => {
+    	//create budget object
+    	let budgetData = res.data.filter(entry => entry.type !== "income" && entry.period === "monthly");
+
+      budgetData = budgetData.map((entry, index) => {
+        let obj = {
+          title: entry.name,
+          index: index
+        }
+        return obj
+        });
+
+    console.log(budgetData);
+
+    this.setState({categories: budgetData}, () => {
+    	console.log(this.state.categories);
+    })})
+
+  	}
   
 
   render() {
@@ -13,9 +43,27 @@ class Modal extends Component {
 	    <div className={showHideClassName}>
 	      <section className="modal-main">
 	        {this.props.children}
-	        <label for="item">Label</label>
-	        <input id="item" name="item" type="text" />
+	        <div className="modal-input">
+	        	<label htmlFor="item">Item</label>
+	        	<input id="item" name="item" type="text" />
+	        </div>
+	        <div className="modal-input">
+	        	<label htmlFor="amount">Amount</label>
+	        	<input id="amount" name="amount" type="text" />
+	        </div>
+	        <div className="modal-input">
+	        	<form>
+	        		<select id="show">
+			        	{this.state.categories.map (category => (
+							<option>{category.title}</option>
+			        	))}
+			        </select>
+		        </form>
+				
+			</div>
+			<div> 
 	        <button onClick={this.props.handleClose}>close</button>
+	        </div>
 	      </section>
 	    </div>
 	  );
