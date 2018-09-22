7a300d59dc4ed306472c2502d374362ff2169582
Christine Bonk <christinebonk@gmail.com>
2018-09-22 12:35:22 -0400

    created account page

--- client/src/pages/Networth/Networth.js
+++ client/src/pages/Networth/Networth.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import TopBar from "../../components/TopBar";
+import API from "../../utils/App.js";
 import { Col, Row, Container } from "../../components/Grid";
+import { Thead, Table, Tbody } from "../../components/Table";
 
 class Networth extends Component {
- 
+
+    state = {
+        accounts: []
+    }
+
+    componentDidMount() {
+        this.searchAccounts();   
+    }
+    
+    searchAccounts = () => {
+        API.searchAccount()
+        .then(res => {
+            console.log(res);
+            this.setState({accounts: res.data});
+        });
+        
+    }
 
     render () { 
         return ( 
@@ -12,7 +30,25 @@ class Networth extends Component {
             <Container>
             	<Row>
             		<Col size="s12">
-                        
+                        {this.state.accounts.length ? (
+                            <Table>
+                              <Thead>
+                                <th>Account</th>
+                                <th>Balance</th>
+                                <th>Category</th>
+                              </Thead>
+                            <Tbody>
+                            {this.state.accounts.map (account => (
+                              <tr key={account.id}>
+                                <td>{account.account}</td>
+                                <td>{account.balance}</td>
+                                <td>{account.type}</td>
+                              </tr>
+                              ))}
+                              </Tbody></Table> )  : (
+                            <h3>You are not on track for Financial Independence</h3>
+                            )
+                          }
             		</Col>
             	</Row>
             </Container>
